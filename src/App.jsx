8b9886f64a62ce5d043776148f2e12a6752bf1db
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-08-21 11:13:25 +0600

    fix(import): update import login and registration as default

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,5 @@
-import { Button } from "./components/ui";
 function App() {
-  return (
-    <div className="p-6">
-      <h1 className="text-3xl font-bold mb-4">Hello Clientflow!</h1>
-
-      <Button variant="primary" fullWidth>
-        Save Client
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
--- src/components/ui/Forms/index.js
+++ src/components/ui/Forms/index.js
@@ -1,1 +1,2 @@
-export { default } from "./Registration";
+export { default as Registration } from "./Registration";
+export { default as Login } from "./Login";
