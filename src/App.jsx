90a69b3029021608493e41a396cdc531486f1e9e
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-09-01 12:39:06 +0600

    feat(auth): update routing to handle registration state and integrate authProvider in main.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,48 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import ProtectedRoute from "./routes/protectedRoute";
+import Dashboard from "./layouts/Dashboad";
+import LoginForm from "./components/Forms/Login";
+import { Registration } from "./components/Forms";
+import { useContext } from "react";
+import { AuthContext } from "./context/authContext";
+
 function App() {
-  return <div></div>;
+  const { user, isRegistered } = useContext(AuthContext);
+  return (
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            user ? (
+              <Navigate to="/dashboard" replace />
+            ) : isRegistered ? (
+              <Navigate to="/login" replace />
+            ) : (
+              <Navigate to="/registration" replace />
+            )
+          }
+        />
+
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/registration" element={<Registration />} />
+
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import App from "./App.jsx";
+import { AuthProvider } from "./context/AuthProvider.jsx";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </StrictMode>,
-)
+);
