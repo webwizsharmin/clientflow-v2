68b1cf68a360ed0b63a43f6128e44ba301bc9d2c
sharmin <178066501+webwizsharmin@users.noreply.github.com>
2026-09-02 12:42:38 +0600

    chore: add boxicons for UI icons

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-
+import "boxicons/css/boxicons.min.css";
 import App from "./App.jsx";
 import { AuthProvider } from "./context/AuthProvider.jsx";
 
